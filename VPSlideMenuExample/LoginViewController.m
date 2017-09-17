ef238da38ef071ad8e09e95722e31b573033db21
Vasily Popov <vasily.popov.it@gmail.com>
2017-09-17 20:15:22 +0300

    change project settings

--- VPSlideMenuExample/LoginViewController.m
+++ VPSlideMenuExample/LoginViewController.m
@@ -38,7 +38,6 @@
     leftVC.mainViewController = nvc;
     
     VPSlideMenuViewController *slideMenuController = [[VPSlideMenuViewController alloc] initWithMainViewController:nvc leftViewController:leftVC rightViewController:rightVC];
-    //slideMenuController.willMenuOverlapMainView = NO;
     [slideMenuController setAutomaticallyAdjustsScrollViewInsets:YES];
     
 
